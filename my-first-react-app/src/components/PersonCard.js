38066e7517ba16089eca328762234af5dcaadb91
achan <achan@achans-MacBook-Pro.local>
2021-03-08 14:22:04 -0800

    adding state and button to PersonCard component (putting it all together assignment)

--- my-first-react-app/src/components/PersonCard.js
+++ my-first-react-app/src/components/PersonCard.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: props.age,
+        }
+    }
+
+    birthday = () =>{
+        this.setState({age: this.state.age + 1});
+    }
+
     render(){
         const { firstName, lastName, age, hairColor } = this.props;
         return (
             <div>
                 <h2>{lastName}, {firstName}</h2>
                 <ul>
-                    <li>Age: {age}</li>
+                    <li>Age: {this.state.age}</li>
                     <li>Hair Color: {hairColor}</li>
                 </ul>
+                <button onClick={ this.birthday }>Birthday button for {firstName} {lastName}</button>
             </div>
         )
     }
+
 }
 
 export default PersonCard
