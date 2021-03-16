ace18c21c844af7e790140815f451b8be769187f
achan <achan@achans-MacBook-Pro.local>
2021-03-16 14:10:57 -0700

    adding Product Manager 1

---
+++ ProductManager/client/src/components/productForm.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import react, { useState } from 'react'
+
+const ProductForm = (props) => {
+    const [newProduct, setNewProduct] = useState({});
+    const [title, setTitle] = useState("");
+    const [number, setNumber] = useState("");
+    const [description, setDescription] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setNewProduct({
+            title: title,
+            price: number,
+            description: description,
+        })
+        console.log(newProduct);
+        axios.post('http://localhost:8000/api/products/new', newProduct)
+        .then( results => {
+            console.log(results);
+        }).catch( err => {
+            console.log(err);
+        })
+    }
+
+    return (
+        <form onSubmit={submitHandler} method="post">
+            <div>
+                <label>
+                    <input type="text" onChange={e=>setTitle(e.target.value)} value={title}></input>
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input type="text" onChange={e=>setNumber(e.target.value)} value={number}></input>
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input type="text" onChange={e=>setDescription(e.target.value)} value={description}></input>
+                </label>
+            </div>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default ProductForm;
