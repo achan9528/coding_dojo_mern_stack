8c936b4e464a465cc83045b0089996bf1465f0c4
achan <achan@achans-MacBook-Pro.local>
2021-03-30 15:19:42 -0700

    brought styles into css files

--- ChatApp/client/src/components/MessageBox.jsx
+++ ChatApp/client/src/components/MessageBox.jsx
@@ -5,26 +5,14 @@ import '../App.css';
 const MessageBox = (props) => {
     const [userMessage, setUserMessage] = useState("");
 
-    const styles = {
-        overflow: "auto",
-        height: "75%",
-        width: "75%",
-        margin: "auto",
-
-    }
-
-    const textareaStyles = {
-        width: "75%",
-        border: "2px solid black",
-        display: "inline-block",
-    }
-
-    const buttonStyles = {
-        verticalAlign: "top",
-        display: "inline-block",
+    const enterListener = (e) =>{
+        if (e.code==="Enter"){
+            document.getElementById("submitMessage").click();
+        }
     }
 
     const submitHandler = (e) => {
+        // alert("triggered the submitHandler");
         e.preventDefault();        
         // for each pasted item, grab each pasted image if any
         let imgSrcArr = [];
@@ -41,39 +29,24 @@ const MessageBox = (props) => {
         });
         // delete each child element
         while (document.getElementById("messgeInputBox")) {
-            // document.getElementById("messgeInputBox").removeChild(
-            //     document.getElementById("messgeInputBox")
-            //         .firstChild
-            // );
-
+            document.getElementById("messgeInputBox").removeChild(
+                document.getElementById("messgeInputBox")
+                    .firstChild
+            );
         }
         // clear the inner text in case there is any message
         // left
         document.getElementById("messageInputBox").innerText = "";
-        // setUserMessage("");
-        
-        
-    }
-
-    const test = (e) => {
-        // e.preventDefault();
-        // e.clipboardData.items grabs the pictures from the div
-        console.log(e.clipboardData);
-        console.log(e.clipboardData.items);
-        console.log(e.clipboardData.items[1]);
-        // console.log(e.clipboardData);
-        // console.log(window);
-
     }
 
     return (
-        <form style={styles} onSubmit={submitHandler}>
-            <div contentEditable={true}
-                onPaste={e => test(e)}
-                style={textareaStyles}
+        <form className="messageInput" onSubmit={submitHandler}>
+            <div
+                contentEditable={true}
                 value={userMessage}
+                onKeyDown={(e)=>{enterListener(e)}}
                 id="messageInputBox"></div>
-            <button style={buttonStyles}>Send</button>
+            <button id="submitMessage">Send</button>
         </form>
     )
 }
--- ChatApp/client/src/components/MessageThread.jsx
+++ ChatApp/client/src/components/MessageThread.jsx
@@ -1,47 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 // import '../App.css';
+import '../ChatApp.css';
 
 const MessageThread = (props) => {
 
     useEffect(() => {
-
-    }, [])
-
-    const styles = {
-        overflowY: "scroll",
-        listStyleType: "none",
-        margin: "auto",
-        padding: "0px",
-        height: "250px",
-        width: "75%",
-    }
-
-    const bubbleEdge = {
-        position: "relative",
-        backgroundColor: "red",
-    }
+        scrollToBottom();
+    }, [props])
 
     const scrollToBottom = () => {
-        props.messages[props.messages.length].scrollIntoView();
+        document.getElementById("dummy").scrollIntoView(
+            true
+        );
     }
 
     const findImages = (src) => {
-        const imgStyle = {
-            maxWidth: "100%",
-            paddingTop: "0%",
-        }
         return (
-            <img style={imgStyle} src={src}></img>
+            <img src={src}></img>
         )
     }
     const consolidateImages = (imgList) =>{
-        const listStyle={
-            listStyleType: "none",
-            padding: "0px",
-        }
         return(
-            <ul style={listStyle}>
+            <ul className="unbulleted-noPadding">
                 {
                     imgList.map((item,key) =>{
                         console.log(item);
@@ -54,63 +35,38 @@ const MessageThread = (props) => {
         )
     }
     return (
-        <ul style={styles}>
+        <ul className="message-thread" id="messageThread" onChange={scrollToBottom}>
             {
                 props.messages.map((item, key) => {
-                    let itemStyle = {}
+                    let styleName = ""
                     if (item.author == props.username) {
-                        itemStyle = {
-                            // backgroundColor: "blue",
-                            // color: "white",
-                            // display: "block",
-                            // verticalAlign: "middle",
-                            // position: "relative",
-                            // left: "25%",
-                            maxWidth: "75%",
-                            textAlign: "left",
-                            // wordWrap: "break-word",
-                            // overflowWrap: "break-word",
-                            // margin: "5px"
-                            // padding: ".5% 1%",
-                        }
+                        styleName="userMessage"
                     } else {
-                        itemStyle = {
-                            backgroundColor: "white",
-                            // display: "inline-block",
-                            // verticalAlign: "middle",
-                            position: "relative",
-                            left: "25%",
-                            maxWidth: "75%",
-                            textAlign: "left",
-                            // padding: ".5% 1%",
-                            // wordWrap: "break-word",
-                            // overflowWrap: "break-word"
-                        }
+                        styleName="otherMessage"
                     }
 
                     let images = []
                     if (item.message.images.length > 0) {
                         // for each image, print an image tag
                         for (let i = 0; i < item.message.images.length; i++) {
-                            console.log(item.message.images[i]);
                             images.push(findImages(item.message.images[i]));
                         }
                     }
-                    console.log(consolidateImages(images));
+                    
                     let test = consolidateImages(images);
                     return (
                         <li key={key}>
-                            {/* {item.author} said: {item.message} */}
-                            <div style={itemStyle}>
+                            <div className={styleName}>
                                 {item.message.text}
                                 {consolidateImages(images)}
-                                {/* <div style={bubbleEdge}></div> */}
                             </div>
-
                         </li>
                     )
                 })
             }
+            <li id="dummy">
+                <div></div>
+            </li>
         </ul>
     )
 }
